498047439d8772ee6cff73e4490545f60cb3fd99
Maks Orlovich <morlovich@google.com>
2012-02-27 22:20:25 +0000

    Fix an embarassing bug: make FileCacheSizeKb actually be interpeted as kb. Might explain some of recent problem reports about rewrite deadlines, since we would rewrite a lot more than needed...

--- src/net/instaweb/apache/apache_cache.cc
+++ src/net/instaweb/apache/apache_cache.cc
@@ -55,7 +55,7 @@ ApacheCache::ApacheCache(const StringPiece& path,
       factory->timer(),
       factory->hasher(),
       config.file_cache_clean_interval_ms(),
-      config.file_cache_clean_size_kb());
+      config.file_cache_clean_size_kb() * 1024);
   file_cache_ = new FileCache(
       config.file_cache_path(), factory->file_system(), NULL,
       factory->filename_encoder(), policy, factory->message_handler());
