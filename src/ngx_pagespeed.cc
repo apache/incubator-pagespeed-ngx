6c8c4c07ca563dcfecba4886ed4e8ec03c5a824e
Jeff Kaufman <jefftk@google.com>
2012-11-02 16:18:42 -0400

    clear etag

--- src/ngx_pagespeed.cc
+++ src/ngx_pagespeed.cc
@@ -609,6 +609,10 @@ ngx_http_pagespeed_header_filter(ngx_http_request_t* r) {
   // and calculate on the fly.
   ngx_http_clear_content_length(r);
 
+  // Pagespeed doesn't need etags: html is always not to be cached while
+  // resources are modified to have an etag-like hash in the url.
+  ngx_http_clear_etag(r);
+
   r->filter_need_in_memory = 1;
 
   int rc = ngx_http_pagespeed_create_request_context(r);
