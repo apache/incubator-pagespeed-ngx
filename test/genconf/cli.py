1e0125bcd85e3ba7c24c33688cf7d9c85bf10084
Otto van der Schaaf <oschaaf@gmail.com>
2012-12-28 23:10:19 +0100

    generate-configuration: save some work
    this saves some work, before refactoring out some common logic
    in the templates

--- test/genconf/cli.py
+++ test/genconf/cli.py
@@ -3,6 +3,13 @@
 import sys
 from genconf import execute_template
 
+def exit_with_help_message():
+    print("usage: ./genconf.py <output_format> <mode>")
+    print("where output_format can be either 'apache' or 'nginx' or 'iis'")
+    print("and mode can be one of @@TODO")
+    quit()
+
+
 conditions = {
   #"ALL_DIRECTIVES":1,
   #"MEMCACHED":1,
@@ -14,17 +21,20 @@ placeholders = {
   "APACHE_DOC_ROOT" : "/drives/c/test/" 
 }
 
+output_format = "nginx"
 mode = ""
 
-if len(sys.argv) == 2:
-    mode = sys.argv[1]
+if len(sys.argv) == 3:
+    output_format = sys.argv[1]
+    mode = sys.argv[2]
 
-if not (mode == "apache" or mode =="nginx" or mode == "iis"):
-    print("usage: ./genconf.py <mode>")
-    print("where mode can be either 'apache' or 'nginx'")
-    quit()
+print("mode:" + mode);
+print("format: " + output_format
+)
+if not (output_format == "apache" or output_format =="nginx" or output_format == "iis"):
+    exit_with_help_message()
 
-template = mode + ".conf.template"
+template = output_format + ".conf.template"
 text = execute_template("pagespeed.pyconf", conditions, placeholders, template)
 
 print text
