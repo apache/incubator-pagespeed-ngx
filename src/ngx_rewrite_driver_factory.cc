3e0a2eaec59202fc723f2cf178aea9b54f7127f5
Otto van der Schaaf <oschaaf@gmail.com>
2013-01-29 21:35:17 +0100

    shared-mem-locks: move comma's from initialiser list to eol

--- src/ngx_rewrite_driver_factory.cc
+++ src/ngx_rewrite_driver_factory.cc
@@ -74,13 +74,13 @@ class Writer;
 const char NgxRewriteDriverFactory::kMemcached[] = "memcached";
 
 NgxRewriteDriverFactory::NgxRewriteDriverFactory(NgxRewriteOptions* main_conf)
-    : RewriteDriverFactory(new NgxThreadSystem())
+    : RewriteDriverFactory(new NgxThreadSystem()),
       // TODO(oschaaf): mod_pagespeed ifdefs this:
-    , shared_mem_runtime_(new PthreadSharedMem())
-    , cache_hasher_(20)
-    , main_conf_(main_conf)
-    , threads_started_(false)
-    , is_root_process_(true) {
+      shared_mem_runtime_(new PthreadSharedMem()),
+      cache_hasher_(20),
+      main_conf_(main_conf),
+      threads_started_(false),
+      is_root_process_(true) {
   RewriteDriverFactory::InitStats(&simple_stats_);
   SerfUrlAsyncFetcher::InitStats(&simple_stats_);
   AprMemCache::InitStats(&simple_stats_);
