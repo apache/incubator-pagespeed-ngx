d8ee5cb5c7ed74042d437fd4d78e1fc6ac880c47
Otto van der Schaaf <oschaaf@gmail.com>
2013-01-31 23:19:17 +0100

    message-handler: don't ignore file and line in FileMessageVImpl

--- src/ngx_message_handler.cc
+++ src/ngx_message_handler.cc
@@ -140,8 +140,9 @@ void NgxMessageHandler::FileMessageVImpl(MessageType type, const char* file,
   ngx_uint_t log_level = GetNgxLogLevel(type);
   GoogleString formatted_message = Format(msg, args);
   if (log_ != NULL) {
-    ngx_log_error(log_level, log_, 0/*ngx_err_t*/, "[%s %s] %s",
-                  kModuleName, kModPagespeedVersion, formatted_message.c_str());
+    ngx_log_error(log_level, log_, 0/*ngx_err_t*/, "[%s %s] %s:%d:%s",
+                  kModuleName, kModPagespeedVersion, file, line,
+                  formatted_message.c_str());
   } else {
     GoogleMessageHandler::FileMessageVImpl(type, file, line, msg, args);
   }
