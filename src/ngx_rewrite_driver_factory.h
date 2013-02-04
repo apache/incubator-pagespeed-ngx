fd6a12df6dd0c9a6cf2cbfdf1dd06cd26b487874
Otto van der Schaaf <oschaaf@gmail.com>
2013-02-04 08:44:42 +0100

    factory-up-front: move comment

--- src/ngx_rewrite_driver_factory.h
+++ src/ngx_rewrite_driver_factory.h
@@ -48,8 +48,6 @@ class NgxRewriteDriverFactory : public RewriteDriverFactory {
   static const char kStaticJavaScriptPrefix[];
   static const char kMemcached[];
 
-  // main_conf will have only options set in the main block.  It may be NULL,
-  // and we do not take ownership.
   explicit NgxRewriteDriverFactory();
   virtual ~NgxRewriteDriverFactory();
   virtual Hasher* NewHasher();
@@ -138,6 +136,8 @@ class NgxRewriteDriverFactory : public RewriteDriverFactory {
   typedef std::map<GoogleString, NgxCache*> PathCacheMap;
   PathCacheMap path_cache_map_;
   MD5Hasher cache_hasher_;
+  // main_conf will have only options set in the main block.  It may be NULL,
+  // and we do not take ownership.
   NgxRewriteOptions* main_conf_;
   typedef std::set<NgxServerContext*> NgxServerContextSet;
   NgxServerContextSet uninitialized_server_contexts_;
