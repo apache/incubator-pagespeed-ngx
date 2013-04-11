d19a819acd8376134305512d05538cd6098d31f4
Jeff Kaufman <jefftk@google.com>
2013-04-11 16:12:27 -0400

    system-test: cdn rewriting

--- test/nginx_system_test.sh
+++ test/nginx_system_test.sh
@@ -1003,6 +1003,30 @@ check [ $ERRS -ge 1 ]
 #check grep "URL http://modpagespeed.com:1023/someimage.png active for " \
 #  $SERF_REFUSED_PATH
 
+# http://code.google.com/p/modpagespeed/issues/detail?id=494 -- test
+# that fetching a css with embedded relative images from a different
+# VirtualHost, accessing the same content, and rewrite-mapped to the
+# primary domain, delivers results that are cached for a year, which
+# implies the hash matches when serving vs when rewriting from HTML.
+#
+# This rewrites the CSS, absolutifying the embedded relative image URL
+# reference based on the the main server host.
+WGET_ARGS=""
+start_test Relative images embedded in a CSS file served from a mapped domain
+DIR="mod_pagespeed_test/map_css_embedded"
+URL="http://www.example.com/$DIR/issue494.html"
+MAPPED_CSS="$DIR/A.styles.css.pagespeed.cf.w9O-kBfMWw.css"
+http_proxy=$SECONDARY_HOSTNAME fetch_until $URL \
+    "grep -c cdn.example.com/$MAPPED_CSS" 1
+
+# Now fetch the resource using a different host, which is mapped to the first
+# one.  To get the correct bytes, matching hash, and long TTL, we need to do
+# apply the domain mapping in the CSS resource fetch.
+URL="http://origin.example.com/$MAPPED_CSS"
+echo http_proxy=$SECONDARY_HOSTNAME $WGET_DUMP $URL
+CSS_OUT=$(http_proxy=$SECONDARY_HOSTNAME $WGET_DUMP $URL)
+check_from "$CSS_OUT" fgrep -q "Cache-Control: max-age=31536000"
+
 run_post_cache_flush
 
 check_failures_and_exit
