6c805a37a3a180b0c1dd12587d9744fdab86ee00
Maks Orlovich <morlovich@google.com>
2013-04-26 18:48:29 +0000

    Attempt to fix RE2 (and hence MPS) build on Windows buildbots by providing a barebones stdint.h for it.

---
+++ src/third_party/re2/win_compat/stdint.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Author: morlovich@google.com (Maksim Orlovich)
+//
+// A tiny subset of C99's stdint.h that we need to build RE2 on Windows.
+
+#ifndef THIRD_PARTY_INSTAWEB_SRC_THIRD_PARTY_RE2_WIN_COMPAT_STDINT_H_
+#define THIRD_PARTY_INSTAWEB_SRC_THIRD_PARTY_RE2_WIN_COMPAT_STDINT_H_
+
+#include "pagespeed/kernel/base/basictypes.h"
+
+// Define C99-style typedefs in terms of Google-style typedefs provided by
+// Chromium, so RE2 can use them to provide Google-style typedefs inside the
+// RE2 namespace.
+
+typedef int8 int8_t;
+typedef uint8 uint8_t;
+typedef int16 int16_t;
+typedef uint16 uint16_t;
+typedef int32 int32_t;
+typedef uint32 uint32_t;
+typedef int64 int64_t;
+typedef uint64 uint64_t;
+
+#endif  // THIRD_PARTY_INSTAWEB_SRC_THIRD_PARTY_RE2_WIN_COMPAT_STDINT_H_
+
