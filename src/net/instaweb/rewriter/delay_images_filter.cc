00f07dbbf6c489560052b05c5637d8761790a430
bharathbhushan@google.com <bharathbhushan@google.com@b28baae1-b17d-8ef2-060a-10057fd0e0fa>
2013-07-01 21:51:11 +0000

    delay_images should remain active if split_html_helper is enabled - since it will fill up the critical image info.

--- src/net/instaweb/rewriter/delay_images_filter.cc
+++ src/net/instaweb/rewriter/delay_images_filter.cc
@@ -241,7 +241,9 @@ void DelayImagesFilter::DetermineEnabled() {
   }
   CriticalImagesFinder* finder =
       driver_->server_context()->critical_images_finder();
-  if (finder->IsMeaningful(driver_) && !finder->IsSetFromPcache(driver_)) {
+  if (finder->IsMeaningful(driver_) &&
+      !finder->IsSetFromPcache(driver_) &&
+      !driver_->options()->Enabled(RewriteOptions::kSplitHtmlHelper)) {
     log_record->LogRewriterHtmlStatus(
         RewriteOptions::FilterId(RewriteOptions::kDelayImages),
         RewriterHtmlApplication::PROPERTY_CACHE_MISS);
