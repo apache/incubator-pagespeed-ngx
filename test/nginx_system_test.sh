261b602f83f3f2bff50102bc12ce4fe0bb204a0c
Jeffrey Crowell <crowell@bu.edu>
2014-04-25 11:48:29 -0400

    add ngx test support for RequestOptionOverride option

--- test/nginx_system_test.sh
+++ test/nginx_system_test.sh
@@ -2371,6 +2371,46 @@ check_from "$OUT" grep -q '<!--'
 check_from "$OUT" grep -q '  '
 WGET_ARGS=""
 
+start_test Request Option Override : Correct values are passed
+HOST_NAME="http://request-option-override.example.com"
+OPTS="?ModPagespeed=on"
+OPTS+="&ModPagespeedFilters=+collapse_whitespace,+remove_comments"
+OPTS+="&PageSpeedRequestOptionOverride=abc"
+URL="$HOST_NAME/mod_pagespeed_test/forbidden.html$OPTS"
+OUT="$(http_proxy=$SECONDARY_HOSTNAME $WGET_DUMP $URL)"
+echo wget $URL
+check_not_from "$OUT" grep -q '<!--'
+
+start_test Request Option Override : Incorrect values are passed
+HOST_NAME="http://request-option-override.example.com"
+OPTS="?ModPagespeed=on"
+OPTS+="&ModPagespeedFilters=+collapse_whitespace,+remove_comments"
+OPTS+="&PageSpeedRequestOptionOverride=notabc"
+URL="$HOST_NAME/mod_pagespeed_test/forbidden.html$OPTS"
+OUT="$(http_proxy=$SECONDARY_HOSTNAME $WGET_DUMP $URL)"
+echo wget $URL
+check_from "$OUT" grep -q '<!--'
+
+start_test Request Option Override : Correct values are passed as headers
+HOST_NAME="http://request-option-override.example.com"
+OPTS="--header=ModPagespeed:on"
+OPTS+=" --header=ModPagespeedFilters:+collapse_whitespace,+remove_comments"
+OPTS+=" --header=PageSpeedRequestOptionOverride:abc"
+URL="$HOST_NAME/mod_pagespeed_test/forbidden.html"
+echo wget $OPTS $URL
+OUT="$(http_proxy=$SECONDARY_HOSTNAME $WGET_DUMP $OPTS $URL)"
+check_not_from "$OUT" grep -q '<!--'
+
+start_test Request Option Override : Incorrect values are passed as headers
+HOST_NAME="http://request-option-override.example.com"
+OPTS="--header=ModPagespeed:on"
+OPTS+=" --header=ModPagespeedFilters:+collapse_whitespace,+remove_comments"
+OPTS+=" --header=PageSpeedRequestOptionOverride:notabc"
+URL="$HOST_NAME/mod_pagespeed_test/forbidden.html"
+echo wget $OPTS $URL
+OUT="$(http_proxy=$SECONDARY_HOSTNAME $WGET_DUMP $OPTS $URL)"
+check_from "$OUT" grep -q '<!--'
+
 start_test JS gzip headers
 
 JS_URL="$HOSTNAME/pagespeed_static/js_defer.$HASH.js"
