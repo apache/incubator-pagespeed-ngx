da716b3bebd5630b91eb7a57f895be6f01fb5402
Josh Marantz <jmarantz@gmail.com>
2014-05-23 14:39:42 +0000

    Add temps for readability and (marginal, theoretical) efficiency.

--- src/net/instaweb/rewriter/rewrite_query.cc
+++ src/net/instaweb/rewriter/rewrite_query.cc
@@ -271,15 +271,17 @@ RewriteQuery::Status RewriteQuery::Scan(
   RequestHeaders::CookieMultimapConstIter it, end;
   for (it = all_cookies.begin(), end = all_cookies.end(); it != end; ++it) {
     GoogleUrl gurl;
-    GoogleString unescaped = GoogleUrl::UnescapeIgnorePlus(it->second.first);
+    StringPiece cookie_name = it->first;
+    StringPiece cookie_value = it->second.first;
+    GoogleString unescaped = GoogleUrl::UnescapeIgnorePlus(cookie_value);
     StringPiece sanitized = SanitizeValueAsQP(unescaped, &gurl);
     GoogleString escaped = GoogleUrl::Escape(sanitized);
-    if (unescaped == sanitized && escaped == it->second.first) {
+    if (unescaped == sanitized && escaped == cookie_value) {
       RequestContextPtr null_request_context;
-      if (ScanNameValue(it->first, unescaped, allow_options,
+      if (ScanNameValue(cookie_name, unescaped, allow_options,
                         null_request_context, request_properties.get(),
                         options_.get(), handler) == kSuccess) {
-        pagespeed_option_cookies_.AddEscaped(it->first, unescaped);
+        pagespeed_option_cookies_.AddEscaped(cookie_name, unescaped);
         status = kSuccess;
       }
       // A PageSpeed cookies with an invalid value will not be cleared. This
@@ -288,7 +290,7 @@ RewriteQuery::Status RewriteQuery::Scan(
     } else {
       handler->Message(kInfo, "Cookie value seems mangled: "
                        "cookie='%s', escaped='%s'",
-                       it->second.first.as_string().c_str(), escaped.c_str());
+                       cookie_value.as_string().c_str(), escaped.c_str());
     }
   }
 
