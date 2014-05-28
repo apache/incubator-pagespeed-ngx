243d4e1931d3ec4088ace3deeb482aab9f6ed780
Jeff Kaufman <jefftk@google.com>
2014-05-28 11:14:14 -0400

    fixup: unbackport the RequestOptionOverride tests from 8246c03f

--- test/nginx_system_test.sh
+++ test/nginx_system_test.sh
@@ -2396,47 +2396,6 @@ test_decent_browsers "New Opera" \
 WGETRC=$OLD_WGETRC
 WGET_ARGS=""
 
-JS_URL="$HOSTNAME/pagespeed_static/js_defer.$HASH.js"
-start_test Request Option Override : Correct values are passed
-HOST_NAME="http://request-option-override.example.com"
-OPTS="?ModPagespeed=on"
-OPTS+="&ModPagespeedFilters=+collapse_whitespace,+remove_comments"
-OPTS+="&PageSpeedRequestOptionOverride=abc"
-URL="$HOST_NAME/mod_pagespeed_test/forbidden.html$OPTS"
-OUT="$(http_proxy=$SECONDARY_HOSTNAME $WGET_DUMP $URL)"
-echo wget $URL
-check_not_from "$OUT" grep -q '<!--'
-
-start_test Request Option Override : Incorrect values are passed
-HOST_NAME="http://request-option-override.example.com"
-OPTS="?ModPagespeed=on"
-OPTS+="&ModPagespeedFilters=+collapse_whitespace,+remove_comments"
-OPTS+="&PageSpeedRequestOptionOverride=notabc"
-URL="$HOST_NAME/mod_pagespeed_test/forbidden.html$OPTS"
-OUT="$(http_proxy=$SECONDARY_HOSTNAME $WGET_DUMP $URL)"
-echo wget $URL
-check_from "$OUT" grep -q '<!--'
-
-start_test Request Option Override : Correct values are passed as headers
-HOST_NAME="http://request-option-override.example.com"
-OPTS="--header=ModPagespeed:on"
-OPTS+=" --header=ModPagespeedFilters:+collapse_whitespace,+remove_comments"
-OPTS+=" --header=PageSpeedRequestOptionOverride:abc"
-URL="$HOST_NAME/mod_pagespeed_test/forbidden.html"
-echo wget $OPTS $URL
-OUT="$(http_proxy=$SECONDARY_HOSTNAME $WGET_DUMP $OPTS $URL)"
-check_not_from "$OUT" grep -q '<!--'
-
-start_test Request Option Override : Incorrect values are passed as headers
-HOST_NAME="http://request-option-override.example.com"
-OPTS="--header=ModPagespeed:on"
-OPTS+=" --header=ModPagespeedFilters:+collapse_whitespace,+remove_comments"
-OPTS+=" --header=PageSpeedRequestOptionOverride:notabc"
-URL="$HOST_NAME/mod_pagespeed_test/forbidden.html"
-echo wget $OPTS $URL
-OUT="$(http_proxy=$SECONDARY_HOSTNAME $WGET_DUMP $OPTS $URL)"
-check_from "$OUT" grep -q '<!--'
-
 start_test JS gzip headers
 
 JS_URL="$HOSTNAME/pagespeed_custom_static/js_defer.$HASH.js"
