357eb92683e5e44d557cdfdcc2a7289277f1d422
Jeff Kaufman <jefftk@google.com>
2014-06-16 17:47:04 -0400

    options: move global option parsing to system/

--- src/ngx_rewrite_options.cc
+++ src/ngx_rewrite_options.cc
@@ -272,84 +272,38 @@ const char* NgxRewriteOptions::ParseAndSetOptions(
     result = ParseAndSetOptions0(directive, &msg, handler);
   } else if (n_args == 2) {
     StringPiece arg = args[1];
-    // TODO(morlovich): Remove these special hacks, and handle these via
-    // ParseAndSetOptionFromEnum1.
-    if (IsDirective(directive, "UsePerVHostStatistics")) {
-      result = ParseAndSetOptionHelper<NgxRewriteDriverFactory>(
-          arg, driver_factory,
-          &SystemRewriteDriverFactory::set_use_per_vhost_statistics);
-    } else if (IsDirective(directive, "InstallCrashHandler")) {
-      result = ParseAndSetOptionHelper<NgxRewriteDriverFactory>(
-          arg, driver_factory,
-          &SystemRewriteDriverFactory::set_install_crash_handler);
-    } else if (IsDirective(directive, "MessageBufferSize")) {
-      int message_buffer_size;
-      bool ok = StringToInt(arg.as_string(), &message_buffer_size);
-      if (ok && message_buffer_size >= 0) {
-        driver_factory->set_message_buffer_size(message_buffer_size);
-        result = RewriteOptions::kOptionOk;
-      } else {
-        result = RewriteOptions::kOptionValueInvalid;
-      }
-    } else if (IsDirective(directive, "UseNativeFetcher")) {
+    if (IsDirective(directive, "UseNativeFetcher")) {
       result = ParseAndSetOptionHelper<NgxRewriteDriverFactory>(
           arg, driver_factory,
           &NgxRewriteDriverFactory::set_use_native_fetcher);
-    } else if (IsDirective(directive, "ForceCaching")) {
-      result = ParseAndSetOptionHelper<SystemRewriteDriverFactory>(
-          arg, driver_factory,
-          &SystemRewriteDriverFactory::set_force_caching);
-    } else if (IsDirective(directive, "ListOutstandingUrlsOnError")) {
-      result = ParseAndSetOptionHelper<SystemRewriteDriverFactory>(
-          arg, driver_factory,
-          &SystemRewriteDriverFactory::list_outstanding_urls_on_error);
-    } else if (IsDirective(directive, "NumRewriteThreads") ||
-               IsDirective(directive, "NumExpensiveRewriteThreads")) {
-      int64 threads = 0;
-      if (!StringToInt64(arg, &threads) || threads < 0) {
-        result = RewriteOptions::kOptionValueInvalid;
-        msg = "must set an positive integer number of threads";
-      } else {
-        if (IsDirective(directive, "NumRewriteThreads")) {
-          driver_factory->set_num_rewrite_threads(threads);
-        } else {
-          driver_factory->set_num_expensive_rewrite_threads(threads);
-        }
-        result = RewriteOptions::kOptionOk;
-      }
-    } else if (IsDirective(directive, "TrackOriginalContentLength")) {
-      result = ParseAndSetOptionHelper<SystemRewriteDriverFactory>(
-          arg, driver_factory,
-          &SystemRewriteDriverFactory::set_track_original_content_length);
-    } else if (IsDirective(directive, "StaticAssetPrefix")) {
-      driver_factory->set_static_asset_prefix(arg);
-      result = RewriteOptions::kOptionOk;
     } else {
       result = ParseAndSetOptionFromName1(directive, arg, &msg, handler);
+      if (result == RewriteOptions::kOptionNameUnknown) {
+        result = driver_factory->ParseAndSetOption1(
+            directive,
+            arg,
+            scope >= RewriteOptions::kProcessScope,
+            &msg,
+            handler);
+      }
     }
   } else if (n_args == 3) {
-    // Short-term special handling, until this moves to common code.
-    // TODO(morlovich): Clean this up.
-    if (StringCaseEqual(directive, "CreateSharedMemoryMetadataCache")) {
-      int64 kb = 0;
-      if (!StringToInt64(args[2], &kb) || kb < 0) {
-        result = RewriteOptions::kOptionValueInvalid;
-        msg = "size_kb must be a positive 64-bit integer";
-      } else {
-        bool ok = driver_factory->caches()->CreateShmMetadataCache(
-            args[1].as_string(), kb, &msg);
-        result = ok ? kOptionOk : kOptionValueInvalid;
-      }
-    } else {
-      result = ParseAndSetOptionFromName2(directive, args[1], args[2],
-                                          &msg, handler);
+    result = ParseAndSetOptionFromName2(directive, args[1], args[2],
+                                        &msg, handler);
+    if (result == RewriteOptions::kOptionNameUnknown) {
+      result = driver_factory->ParseAndSetOption2(
+          directive,
+          args[1],
+          args[2],
+          scope >= RewriteOptions::kProcessScope,
+          &msg,
+          handler);
     }
   } else if (n_args == 4) {
     result = ParseAndSetOptionFromName3(
         directive, args[1], args[2], args[3], &msg, handler);
   } else {
-    return ps_error_string_for_option(
-        pool, directive, "not recognized or too many arguments");
+    result = RewriteOptions::kOptionNameUnknown;
   }
 
   switch (result) {
