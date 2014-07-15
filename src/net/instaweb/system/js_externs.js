bec7b4581d3fb6550ed4b11a5a6109c07f84b7bb
xqyin <xqyin11@gmail.com>
2014-07-15 14:59:09 +0000

    Use open sourced externs file for google.visualization API. Remove the duplicate listing in js_externs.js file.

--- src/net/instaweb/system/js_externs.js
+++ src/net/instaweb/system/js_externs.js
@@ -15,12 +15,12 @@
  */
 
 /**
- * @fileoverview  List of all external objects/functions referenced in
- * console.js and graphs_js.js, but which are actually defined in
- * some external file (in this case in https://www.google.com/jsapi).
- * Since we are not compiling that file we need all those objects/functions
- * to keep the same name. Listing them here accomplishes that.
- *
+ * @fileoverview  List of all external objects/functions referenced in js
+ * flies under system/ but which are actually defined in some external file
+ * (in this case in https://www.google.com/jsapi). Since we are not compiling
+ * that file we need all those objects/functions to keep the same name. Listing
+ * them here accomplishes that. This list is only used by console.js currently
+ * but we could add externs needed by other files here in the future.
  *
  * @author sligocki@google.com (Shawn Ligocki)
  *
@@ -34,83 +34,3 @@ var pagespeedStatisticsUrl = '';
 
 /** @param {function()} callback */
 google.setOnLoadCallback = function(callback) {};
-
-
-/** @const */
-google.visualization = {};
-
-
-
-/**
- * @param {(string|Object)=} opt_data
- * @param {number=} opt_version
- * @constructor
- */
-google.visualization.DataTable = function(opt_data, opt_version) {};
-
-
-/**
- * @param {string|!Object} type
- * @param {string=} opt_label
- * @param {string=} opt_id
- * @return {number}
- */
-google.visualization.DataTable.prototype.addColumn =
-    function(type, opt_label, opt_id) {};
-
-
-/**
- * @param {!Array=} opt_cellArray
- * @return {number}
- */
-google.visualization.DataTable.prototype.addRow = function(opt_cellArray) {};
-
-
-/**
- * @param {number|Array} numOrArray
- * @return {number}
- */
-google.visualization.DataTable.prototype.addRows = function(numOrArray) {};
-
-
-/** @return {number} */
-google.visualization.DataTable.prototype.getNumberOfRows = function() {};
-
-
-
-/** @interface */
-google.visualization.IChart = function() {};
-
-
-/**
- * @param {!Object} data
- * @param {Object=} opt_options
- * @return {undefined}
- */
-google.visualization.IChart.prototype.draw = function(data, opt_options) {};
-
-
-
-/**
- * @param {Node} container
- * @constructor
- * @implements {google.visualization.IChart}
- */
-google.visualization.LineChart = function(container) {};
-
-
-/** @override */
-google.visualization.LineChart.prototype.draw = function(data, opt_options) {};
-
-
-
-/**
- * @param {Node} container
- * @constructor
- * @implements {google.visualization.IChart}
- */
-google.visualization.PieChart = function(container) {};
-
-
-/** @override */
-google.visualization.PieChart.prototype.draw = function(data, opt_options) {};
