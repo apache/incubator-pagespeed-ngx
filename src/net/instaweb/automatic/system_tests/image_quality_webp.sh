0023ea3487a1090619b98c392cbaa5aedf141fc0
Jeff Kaufman <jeff@jefftk.com>
2014-12-06 01:28:21 +0000

    Fix test flake by switching to fetch_until

--- src/net/instaweb/automatic/system_tests/image_quality_webp.sh
+++ src/net/instaweb/automatic/system_tests/image_quality_webp.sh
@@ -4,10 +4,13 @@ mkdir $OUTDIR
 IMG_REWRITE="$TEST_ROOT/webp_rewriting/rewrite_images.html"
 REWRITE_URL="$IMG_REWRITE?PageSpeedFilters=rewrite_images"
 URL="$REWRITE_URL,convert_jpeg_to_webp&$IMAGES_QUALITY=75&$WEBP_QUALITY=65"
-check run_wget_with_args \
-  --header 'X-PSA-Blocking-Rewrite: psatest' --user-agent=webp $URL
+fetch_until -save -recursive $URL \
+  'fgrep -c 256x192xPuzzle.jpg.pagespeed.ic' 1 \
+   --user-agent=webp
 check_file_size "$WGET_DIR/*256x192*Puzzle*webp" -le 5140   # resized, webp'd
+
 rm -rf $WGET_DIR
-check run_wget_with_args \
-  --header 'X-PSA-Blocking-Rewrite: psatest' --header='Accept: image/webp' $URL
+fetch_until -save -recursive $URL \
+  'fgrep -c 256x192xPuzzle.jpg.pagespeed.ic' 1 \
+  --header='Accept:image/webp'
 check_file_size "$WGET_DIR/*256x192*Puzzle*webp" -le 5140   # resized, webp'd
