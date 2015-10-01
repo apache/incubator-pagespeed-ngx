f1bb2f6d243d3e9ce4bb2bfeefb0ecd23092aa90
Jeffrey Crowell <jcrowell@google.com>
2015-10-01 10:24:32 -0400

    sync genfiles

--- net/instaweb/genfiles/rewriter/lazyload_images_dbg.js
+++ net/instaweb/genfiles/rewriter/lazyload_images_dbg.js
@@ -6,7 +6,7 @@
     if (window.ActiveXObject) {
       try {
         d = new ActiveXObject("Msxml2.XMLHTTP");
-      } catch (e) {
+      } catch (f) {
         try {
           d = new ActiveXObject("Microsoft.XMLHTTP");
         } catch (g) {
@@ -17,8 +17,8 @@
   if (!d) {
     return !1;
   }
-  var f = -1 == a.indexOf("?") ? "?" : "&";
-  a = a + f + "url=" + encodeURIComponent(b);
+  var e = -1 == a.indexOf("?") ? "?" : "&";
+  a = a + e + "url=" + encodeURIComponent(b);
   d.open("POST", a);
   d.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
   d.send(c);
