706577a34be57b5d50f6115a3c44f010a3b7993c
Joshua Marantz <jmarantz@gmail.com>
2015-12-02 08:27:47 -0500

    do not consider it a test failure to get a slow ReadFile operation.

--- test/nginx_system_test.sh
+++ test/nginx_system_test.sh
@@ -1309,6 +1309,7 @@ OUT=$(cat "$ERROR_LOG" \
     | grep -v "\\[error\\].*opening temp file: No such file or directory.*" \
     | grep -v "\\[error\\].*remote\.cfg.*" \
     | grep -v "\\[error\\].*Slow read operation on file.*" \
+    | grep -v "\\[error\\].*Slow ReadFile operation on file.*" \
     | grep -v "\\[error\\].*Slow write operation on file.*" \
     | grep -v "\\[warn\\].*remote\.cfg.*" \
     | grep -v "\\[warn\\].*end token not received.*" \
