bd61c0d786b16224dc9bb3316445cb609c3f037d
Joshua Marantz <jmarantz@gmail.com>
2016-01-27 15:34:12 -0500

    That previous attempt borked the quoting.  It's a pain to get the quoting right in fetch_until, so it's better to search for a single word and then afterward grep for the phrase you are looking for.

--- pagespeed/system/system_test.sh
+++ pagespeed/system/system_test.sh
@@ -2285,7 +2285,8 @@ fi
 
 start_test AddResourceHeaders works for pagespeed resources.
 URL="$TEST_ROOT/compressed/hello_js.custom_ext.pagespeed.ce.HdziXmtLIV.txt"
-HTML_HEADERS=$($WGET_DUMP $URL)
+fetch_until -save "$URL" 'fgrep -c text/javascript' 1 --save-headers
+HTML_HEADERS=$(extract_headers $FETCH_FILE)
 check_from "$HTML_HEADERS" grep -q "^X-Foo: Bar"
 
 start_test long url handling
