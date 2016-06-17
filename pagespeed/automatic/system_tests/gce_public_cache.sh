4b7e84a84eea9fcbdd1c60a3d034193720b705c1
Jeffrey Crowell <jcrowell@google.com>
2016-06-17 16:31:02 -0400

    add gce_public_cache.sh system test

---
+++ pagespeed/automatic/system_tests/gce_public_cache.sh
@@ -0,0 +1,77 @@
+YELLOW="$EXAMPLE_ROOT/styles/yellow.css"
+PUBYELLOW="$TEST_ROOT/public/yellow.css"
+
+# Checks whether a fetching a file, fetched with or without a Via header,
+# results in a cache-control header with "public".
+# Usage:
+#  check_public_cc FILE [-via] public|no
+function check_public_cc() {
+  file="$1"
+  shift
+
+  if [ "$1" = -via ]; then
+    echo fetching $file with Via
+    cache_control=$($WGET_DUMP --header "Via: 1.1 google" "$file" | \
+      scrape_header Cache-Control)
+    shift
+  else
+    echo fetching $file without Via
+    cache_control=$($WGET_DUMP "$file" | scrape_header Cache-Control)
+  fi
+
+  if [ "$1" = "public" ]; then
+    check_from "$cache_control" fgrep -q public
+  else
+    check_not_from "$cache_control" fgrep -q public
+  fi
+}
+
+# Takes an HTML CSS link and returns the URL of the CSS file.
+function scrape_css_link() {
+  tr -s '[:space:]' '\n' | grep href | cut -d= -f2 | cut '-d"' -f2
+}
+
+# For combined resources, identified with the correct hash, we do not have
+# to wait for the cache to be warm to expect optimized output with proper
+# headers.  This is because the HTML document requires the combined file; we
+# can't fall back to individual files.
+start_test Cache-Control:public added iff GCE for combined .pagespeed. file.
+fetch_until -save \
+  $EXAMPLE_ROOT/combine_css.html?PageSpeedFilters=combine_css \
+  "fgrep -c .pagespeed.cc" 1
+COMBINED=$(scrape_css_link < $FETCH_FILE)
+check_public_cc "$COMBINED" no
+# TODO(jmarantz): change to "public" when Via:*google is supported.
+check_public_cc "$COMBINED" -via no
+check_public_cc "$COMBINED" no
+# TODO(jmarantz): change to "public" when Via:*google is supported.
+check_public_cc "$COMBINED" -via no
+
+# We won't add 'public' to an ipro-request until the request is optimized, so
+# wait for that to happen.  Then we can check an ipro-rewritten resource to make
+# sure it gets the proper headers.
+start_test Cache-Control:public added iff GCE for ipro css file.
+fetch_until "$YELLOW" 'fgrep -c background-color:#ff0' 1
+check_public_cc "$YELLOW" no
+# TODO(jmarantz): change to "public" when Via:*google is supported.
+check_public_cc "$YELLOW" -via no
+check_public_cc "$YELLOW" no
+# TODO(jmarantz): change to "public" when Via:*google is supported.
+check_public_cc "$YELLOW" -via no
+
+start_test Cache-Control:public when source has cc:public, for ipro
+fetch_until "$PUBYELLOW" 'fgrep -c background-color:#ff0' 1
+check_public_cc "$PUBYELLOW" -via public
+check_public_cc "$PUBYELLOW" public
+check_public_cc "$PUBYELLOW" -via public
+check_public_cc "$PUBYELLOW" public
+
+start_test Cache-Control:public when source has cc:public, for .pagespeed.
+fetch_until -save \
+  $TEST_ROOT/public/rewrite_css.html?PageSpeedFilters=rewrite_css \
+  "fgrep -c .pagespeed.cf" 1
+REWRITTEN_YELLOW=$(scrape_css_link < $FETCH_FILE)
+check_public_cc "$REWRITTEN_YELLOW" -via public
+check_public_cc "$REWRITTEN_YELLOW" public
+check_public_cc "$REWRITTEN_YELLOW" -via public
+check_public_cc "$REWRITTEN_YELLOW" public
