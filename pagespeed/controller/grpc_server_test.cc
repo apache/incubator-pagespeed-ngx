91c7e8eb7eafb92a5bb7627246cd897dcbd0a0e4
Steve Hill <cheesy@google.com>
2016-09-15 10:29:03 -0400

    Move GrpcServerTest implementation into the .cc.

--- pagespeed/controller/grpc_server_test.cc
+++ pagespeed/controller/grpc_server_test.cc
@@ -14,9 +14,122 @@
 //
 // Author: cheesy@google.com (Steve Hill)
 
+#include <sys/time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <grpc++/alarm.h>
+
+#include <memory>
+
+#include "base/logging.h"
+#include "pagespeed/controller/central_controller_rpc_server.h"
+#include "pagespeed/kernel/base/abstract_mutex.h"
+#include "pagespeed/kernel/base/basictypes.h"
+#include "pagespeed/kernel/base/function.h"
+#include "pagespeed/kernel/base/thread.h"
+#include "pagespeed/kernel/base/thread_annotations.h"
+#include "pagespeed/kernel/base/thread_system.h"
+#include "pagespeed/kernel/util/grpc.h"
+#include "pagespeed/kernel/util/platform.h"
+
 #include "pagespeed/controller/grpc_server_test.h"
 
+
 namespace net_instaweb {
+
+// Fixture for testing gRPC servers. Contains an event dispatcher thread, plus
+// some other bits you'll need.
+GrpcServerTest::GrpcServerTest()
+    : thread_system_(Platform::CreateThreadSystem()) {}
+
+GrpcServerTest::~GrpcServerTest() { StopServer(); }
+
+void GrpcServerTest::SetUp() {
+  ::grpc::ServerBuilder builder;
+  builder.AddListeningPort(ServerAddress(), ::grpc::InsecureServerCredentials(),
+                           &listen_port_);
+  queue_ = builder.AddCompletionQueue();
+  RegisterServices(&builder);
+  server_ = builder.BuildAndStart();
+  CHECK(server_ != nullptr);
+  // listen_port_ may have been 0 first time through. gRPC sets -1 on failure.
+  CHECK_GT(listen_port_, 0);
+
+  server_thread_.reset(
+      new GrpcServerThread(queue_.get(), thread_system_.get()));
+  CHECK(server_thread_->Start());
+}
+
+void GrpcServerTest::StopServer() {
+  if (server_thread_ != nullptr) {
+    server_->Shutdown();
+    server_thread_->Stop();
+    server_thread_.reset();
+    server_.reset();
+  }
+}
+
+// Have the server thread call Run() (never Cancel()) on the supplied
+// function, ASAP. This call is thread-safe.
+void GrpcServerTest::QueueFunctionForServerThread(Function* func) {
+  // Function to schedule an alarm that invokes CallRun ASAP on a supplied
+  // function.
+  class DelayedCallFunction : public Function {
+   public:
+    DelayedCallFunction(ThreadSystem* thread_system,
+                        ::grpc::CompletionQueue* queue, Function* function)
+        : mutex_(thread_system->NewMutex()), func_(function) {
+      // The annotations don't require it, but it's important that we take
+      // a lock before creating the alarm; We can immediately get a callback
+      // on the other thread, before alarm_ is properly populated.
+      ScopedMutex lock(mutex_.get());
+      alarm_.reset(
+          new ::grpc::Alarm(queue, gpr_inf_past(GPR_CLOCK_MONOTONIC), this));
+    }
+    ~DelayedCallFunction() override {}
+
+    void Run() override {
+      ScopedMutex lock(mutex_.get());
+      func_->CallRun();
+    }
+
+    void Cancel() override {
+      ScopedMutex lock(mutex_.get());
+      func_->CallRun();
+    }
+
+   private:
+    std::unique_ptr<AbstractMutex> mutex_;
+    Function* func_ GUARDED_BY(mutex_);
+    std::unique_ptr<::grpc::Alarm> alarm_ GUARDED_BY(mutex_);
+
+    DISALLOW_COPY_AND_ASSIGN(DelayedCallFunction);
+  };
+
+  // Schedule the call. This will clean itself up.
+  new DelayedCallFunction(thread_system_.get(), queue_.get(), func);
+}
+
+/* static */ void GrpcServerTest::SetUpTestCase() {
+    // When this is 0, gRPC will pick an unused port for us.
+    listen_port_ = 0;
+}
+
+GrpcServerTest::GrpcServerThread::GrpcServerThread(
+    ::grpc::CompletionQueue* queue, ThreadSystem* thread_system)
+    : Thread(thread_system, "grpc_test_server", ThreadSystem::kJoinable),
+      queue_(queue) {}
+
+GrpcServerTest::GrpcServerThread::~GrpcServerThread() {}
+
+void GrpcServerTest::GrpcServerThread::Stop() {
+  queue_->Shutdown();
+  this->Join();
+}
+
+void GrpcServerTest::GrpcServerThread::Run() {
+  CentralControllerRpcServer::MainLoop(queue_);
+}
 
 int GrpcServerTest::listen_port_;
 
--- pagespeed/controller/grpc_server_test.h
+++ pagespeed/controller/grpc_server_test.h
@@ -17,26 +17,15 @@
 #ifndef PAGESPEED_CONTROLLER_GRPC_SERVER_TEST_H_
 #define PAGESPEED_CONTROLLER_GRPC_SERVER_TEST_H_
 
-#include <sys/time.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <grpc++/alarm.h>
-
 #include <memory>
 
-#include "base/logging.h"
-#include "pagespeed/controller/central_controller_rpc_server.h"
-#include "pagespeed/kernel/base/abstract_mutex.h"
-#include "pagespeed/kernel/base/basictypes.h"
 #include "pagespeed/kernel/base/function.h"
 #include "pagespeed/kernel/base/gtest.h"
 #include "pagespeed/kernel/base/string.h"
 #include "pagespeed/kernel/base/string_util.h"
 #include "pagespeed/kernel/base/thread.h"
-#include "pagespeed/kernel/base/thread_annotations.h"
 #include "pagespeed/kernel/base/thread_system.h"
 #include "pagespeed/kernel/util/grpc.h"
-#include "pagespeed/kernel/util/platform.h"
 
 namespace net_instaweb {
 
@@ -44,85 +33,20 @@ namespace net_instaweb {
 // some other bits you'll need.
 class GrpcServerTest : public testing::Test {
  public:
-  GrpcServerTest() : thread_system_(Platform::CreateThreadSystem()) {
-  }
+  GrpcServerTest();
+  virtual ~GrpcServerTest();
 
-  virtual ~GrpcServerTest() {
-    StopServer();
-  }
+  // Binds server_ to the address returned by ListenAddress().
+  void SetUp() override;
 
-  void SetUp() override {
-    ::grpc::ServerBuilder builder;
-    builder.AddListeningPort(ServerAddress(),
-                             ::grpc::InsecureServerCredentials(),
-                             &listen_port_);
-    queue_ = builder.AddCompletionQueue();
-    RegisterServices(&builder);
-    server_ = builder.BuildAndStart();
-    CHECK(server_.get() != nullptr);
-    // listen_port_ may have been 0 first time through. gRPC sets -1 on failure.
-    CHECK_GT(listen_port_, 0);
-
-    server_thread_.reset(
-        new GrpcServerThread(queue_.get(), thread_system_.get()));
-    CHECK(server_thread_->Start());
-  }
-
-  // Blocks until shutdown is complete.
-  void StopServer() {
-    if (server_thread_ != nullptr) {
-      server_->Shutdown();
-      server_thread_->Stop();
-      server_thread_.reset();
-      server_.reset();
-    }
-  }
+  // Blocks until server_ is stopped.
+  void StopServer();
 
   // Have the server thread call Run() (never Cancel()) on the supplied
   // function, ASAP. This call is thread-safe.
-  void QueueFunctionForServerThread(Function* func) {
-    // Function to schedule an alarm that invokes CallRun ASAP on a supplied
-    // function.
-    class DelayedCallFunction : public Function {
-     public:
-      DelayedCallFunction(ThreadSystem* thread_system,
-                          ::grpc::CompletionQueue* queue, Function* function)
-          : mutex_(thread_system->NewMutex()), func_(function) {
-        // The annotations don't require it, but it's important that we take
-        // a lock before creating the alarm; We can immediately get a callback
-        // on the other thread, before alarm_ is properly populated.
-        ScopedMutex lock(mutex_.get());
-        alarm_.reset(
-            new ::grpc::Alarm(queue, gpr_inf_past(GPR_CLOCK_MONOTONIC), this));
-      }
-      virtual ~DelayedCallFunction() { }
+  void QueueFunctionForServerThread(Function* func);
 
-      void Run() override {
-        ScopedMutex lock(mutex_.get());
-        func_->CallRun();
-      }
-
-      void Cancel() override {
-        ScopedMutex lock(mutex_.get());
-        func_->CallRun();
-      }
-
-     private:
-      std::unique_ptr<AbstractMutex> mutex_;
-      Function* func_ GUARDED_BY(mutex_);
-      std::unique_ptr<::grpc::Alarm> alarm_ GUARDED_BY(mutex_);
-
-      DISALLOW_COPY_AND_ASSIGN(DelayedCallFunction);
-    };
-
-    // Schedule the call. This will clean itself up.
-    new DelayedCallFunction(thread_system_.get(), queue_.get(), func);
-  }
-
-  static void SetUpTestCase() {
-    // When this is 0, gRPC will pick an unused port for us.
-    listen_port_ = 0;
-  }
+  static void SetUpTestCase();
 
   GoogleString ServerAddress() const {
     return StrCat("localhost:", IntegerToString(listen_port_));
@@ -148,24 +72,17 @@ class GrpcServerTest : public testing::Test {
   class GrpcServerThread : public ThreadSystem::Thread {
    public:
     GrpcServerThread(::grpc::CompletionQueue* queue,
-                     ThreadSystem* thread_system)
-        : Thread(thread_system, "grpc_test_server", ThreadSystem::kJoinable),
-          queue_(queue) {}
-    virtual ~GrpcServerThread() {}
-
-    void Stop() {
-      queue_->Shutdown();
-      this->Join();
-    }
+                     ThreadSystem* thread_system);
+    virtual ~GrpcServerThread();
+    void Stop();
 
    private:
-    void Run() override {
-      CentralControllerRpcServer::MainLoop(queue_);
-    }
+    void Run() override;
 
     ::grpc::CompletionQueue* queue_;
   };
 
+  // Override this to call RegisterService on the ServerBuilder.
   virtual void RegisterServices(::grpc::ServerBuilder*) = 0;
 
   static int listen_port_;
