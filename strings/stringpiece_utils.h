fb3ef57e902e16fceb2486e4b9d956155966fa05
Joshua Marantz <jmarantz@gmail.com>
2016-09-23 11:15:55 -0400

    Add a temporary dummy header file to make an internal refactoring process go more smoothly.

---
+++ strings/stringpiece_utils.h
@@ -0,0 +1,6 @@
+// This is a placeholder to allow include-path compatibility with code written
+// inside Google.
+
+#ifndef STRINGS_STRINGPIECE_UTILS_H_
+#define STRINGS_STRINGPIECE_UTILS_H_
+#endif  // STRINGS_STRINGPIECE_UTILS_H_
