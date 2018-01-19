2e202e5c8decb1ad00dc75d2762606bacea8bdbf
Otto van der Schaaf <oschaaf@we-amp.com>
2018-01-19 11:21:10 +0100

    Licensing: Changes for ASF license compliance (#1528)
    * Update/add license banners
    * Strip "Author: " comments
    * Initialize NOTICE

--- scripts/pagespeed_libraries_generator.sh
+++ scripts/pagespeed_libraries_generator.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-#
-# Copyright 2013 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 # Converts pagespeed_libraries.conf from Apache-format to Nginx-format,
 # supporting the canonicalize_javascript_libraries filter.
 # Inspired by https://github.com/apache/incubator-pagespeed-ngx/issues/532
@@ -23,9 +25,6 @@
 #
 # Then have nginx include that configuration file and enable the filter
 # canonicalize_javascript_libraries.
-#
-# Author: vid@zippykid.com (Vid Luther)
-#         jefftk@google.com (Jeff Kaufman)
 
 URL="https://github.com/apache/incubator-pagespeed-mod/raw/master/"
 URL+="net/instaweb/genfiles/conf/pagespeed_libraries.conf"
